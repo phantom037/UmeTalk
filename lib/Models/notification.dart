4e7512ab9287d30a071e1a7dd0e65686d30fc3b7
Leo Tran <58055271+phantom037@users.noreply.github.com>
2022-05-14 17:37:27 +0700

    Update notification.dart

--- lib/Models/notification.dart
+++ lib/Models/notification.dart
@@ -8,15 +8,6 @@ class NotificationAPI {
   static final onNotification = BehaviorSubject<String?>();
 
   static Future _notificationDetails() async {
-    /*
-    final largeIconPath = await Utils.downloadFile("https://dlmocha.com/GameImages/MellowSik.PNG");
-
-    final styleInformation = BigPictureStyleInformation(
-        FilePathAndroidBitmap(bigPicturePath),
-        largeIcon: FilePathAndroidBitmap(largeIconPath));
-
-     */
-
     return NotificationDetails(
       android: AndroidNotificationDetails(
         'full screen channel id', 'full screen channel name',
@@ -80,85 +71,3 @@ class NotificationAPI {
   static void cancel(int id) => _notifications.cancel(id);
   static void cancelAll() => _notifications.cancelAll();
 }
-
-///Notification handle
-/*
-                SizedBox(
-                  height: 30,
-                ),
-                Material(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                  elevation: 5.0,
-                  child: Container(
-                    width: 300.0,
-                    child: MaterialButton(
-                        child: Text(
-                          "Notification",
-                          style: TextStyle(color: Colors.black, fontSize: 16.0),
-                        ),
-                        textColor: Colors.white,
-                        padding: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 10.0),
-                        onPressed: () => NotificationAPI.showNotification(
-                            title: "Ume_Talk",
-                            body: "Hello world",
-                            payload: "Yeah")),
-                    margin: EdgeInsets.only(bottom: 1.0),
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Material(
-                  //Scheduled Notification
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                  elevation: 5.0,
-                  child: Container(
-                    width: 300.0,
-                    child: MaterialButton(
-                        child: Text(
-                          "Scheduled Notification",
-                          style: TextStyle(color: Colors.black, fontSize: 16.0),
-                        ),
-                        textColor: Colors.white,
-                        padding: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 10.0),
-                        onPressed: () =>
-                            NotificationAPI.showScheduledNotification(
-                              title: "Ume_Talk",
-                              body: "Hello world",
-                              payload: "Yeah",
-                              scheduleDate:
-                                  DateTime.now().add(Duration(seconds: 2)),
-                            )),
-                    margin: EdgeInsets.only(bottom: 1.0),
-                  ),
-                ),
-
-                SizedBox(
-                  height: 30,
-                ),
-                Material(
-                  //Scheduled Notification
-                  color: Colors.yellowAccent,
-                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                  elevation: 5.0,
-                  child: Container(
-                    width: 300.0,
-                    child: MaterialButton(
-                        child: Text(
-                          "FCM Notification",
-                          style: TextStyle(color: Colors.black, fontSize: 16.0),
-                        ),
-                        textColor: Colors.black,
-                        padding: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 10.0),
-                        onPressed: () =>
-                            _fcmNotificationService.sendNotificationToUser(
-                              title: 'From Ume Talk',
-                              body: 'You received a new message',
-                              fcmToken: deviceToken,
-                            )),
-                    margin: EdgeInsets.only(bottom: 1.0),
-                  ),
-                ),
-                 */
